5afc23644f608d48ba68372b5da72d137280a89d
stwarte Learte <78548370+stwartepl@users.noreply.github.com>
2021-07-24 10:46:59 -0300

    Desafios Python do Curso em Video
    .py

---
+++ TerceiroDesafioDePython.py
@@ -0,0 +1,9 @@
+#Desafio três
+#escrever um script que leia dois números e tente mostrar a soma entre eles
+
+number1 = int(input('Digite um numero: '))
+number2 = int(input('Digite um numero: '))
+
+soma = number1 + number2
+
+print('A soma de', number1, '+', number2, 'é igual a ',soma)
