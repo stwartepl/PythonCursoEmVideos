ce2387b2518ad30fe9ff699b812ca3270433e78d
stwarte Learte <78548370+stwartepl@users.noreply.github.com>
2021-07-24 10:45:30 -0300

    Primeiros desafios em Python
    .py

---
+++ SegundoDesafioDePython.py
@@ -0,0 +1,11 @@
+#Desafio dois
+#Criar um script Python que leia dia, mês e o ano de uma pessoa e mostre
+#uma mensagem com a data formatada.
+
+nome = input('Qual seu nome? ')
+dia = input('Qual dia do seu nascimento? ')
+mes = input('Em que mês você nasceu? ')
+ano = input('Em que você nasceu? ')
+
+print('Prazer em lhe conhecer',',',nome,'!','Você nasceu no dia',dia,'de',mes,'do ano de',ano,'.')
+
